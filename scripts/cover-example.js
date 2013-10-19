ae6371c44130dedf3407dfcc7c7861531a3aaa5f
captdmd <jacobkeith@Jacobs-MacBook-Pro.local>
2013-10-19 01:01:29 -0400

    bigassimage

--- scripts/cover-example.js
+++ scripts/cover-example.js
@@ -6,9 +6,12 @@ require([
 
   var doCoverForAlbum = function() {
     var album = models.Album.fromURI('spotify:album:2mCuMNdJkoyiXFhsQCLLqw');
-    var image = Image.forAlbum(album, {width: 200, height: 200, player: true});
+    var image = Image.forAlbum(album, {width: 1000, height: 1000, player: true});
     document.getElementById('albumCoverContainer').appendChild(image.node);
   };
+
+
+
 
   exports.doCoverForAlbum = doCoverForAlbum;
 });
